0d76fe2bf895845071f2660b1a8e4691c0ca6460
Strikeskids <github@strikeskids.com>
2013-12-11 17:35:36 -0500

    Added toString method to wrapper

--- src/com/sk/wrappers/Wrapper.java
+++ src/com/sk/wrappers/Wrapper.java
@@ -1,5 +1,8 @@
 package com.sk.wrappers;
 
+import java.lang.reflect.Field;
+import java.util.Arrays;
+
 import com.sk.datastream.Stream;
 
 public abstract class Wrapper<T extends WrapperLoader> {
@@ -20,5 +23,34 @@ public abstract class Wrapper<T extends WrapperLoader> {
 
 	public T getLoader() {
 		return loader;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder output = new StringBuilder();
+		output.append(getClass().getSimpleName());
+		output.append(getId());
+		output.append(" {");
+		for (Field f : getClass().getDeclaredFields()) {
+			Object o;
+			try {
+				o = f.get(this);
+			} catch (IllegalArgumentException | IllegalAccessException e) {
+				continue;
+			}
+			if (o == null)
+				continue;
+			output.append(f.getName());
+			output.append(": ");
+			if (o instanceof Object[]) {
+				output.append(Arrays.toString((Object[]) o));
+			} else {
+				output.append(o);
+			}
+			output.append(", ");
+		}
+		output.delete(output.length() - 2, output.length());
+		output.append("}");
+		return output.toString();
 	}
 }
