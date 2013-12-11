b5def9c2d97c1b00b0464be1485e7a113f53d655
Strikeskids <github@strikeskids.com>
2013-12-11 17:28:38 -0500

    Added object definitions

---
+++ src/com/sk/wrappers/ObjectDefinitionLoader.java
@@ -0,0 +1,40 @@
+package com.sk.wrappers;
+
+import com.sk.cache.fs.Archive;
+import com.sk.cache.fs.CacheSystem;
+import com.sk.cache.fs.CacheType;
+import com.sk.cache.fs.FileData;
+
+public class ObjectDefinitionLoader extends WrapperLoader {
+
+	private final CacheType cache;
+
+	public ObjectDefinitionLoader(CacheSystem cacheSystem) {
+		super(cacheSystem);
+		this.cache = cacheSystem.getCacheType(16);
+	}
+
+	@Override
+	public ObjectDefinition load(int id) {
+		FileData data = loadData(id);
+		ObjectDefinition ret = new ObjectDefinition(this, id);
+		ret.decode(data.getDataAsStream());
+		fixObject(ret);
+		return ret;
+	}
+
+	public FileData loadData(int id) {
+		Archive archive = cache.getArchive(id >>> 8);
+		if (archive == null)
+			throw new IllegalArgumentException("Bad object id");
+		FileData data = archive.getFile(id & 0xff);
+		if (data == null)
+			throw new IllegalArgumentException("Bad object id");
+		return data;
+	}
+
+	private void fixObject(ObjectDefinition ret) {
+		ret.walkable &= !ret.walkable2;
+	}
+
+}
