c36abd469e58da46b4416438fde2b00569779a9f
Strikeskids <github@strikeskids.com>
2013-12-12 18:42:02 -0500

    Add RegionDataLoader

--- src/com/sk/wrappers/LocalObjectLoader.java
+++ src/com/sk/wrappers/LocalObjectLoader.java
@@ -1,48 +1,19 @@
 package com.sk.wrappers;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import com.sk.cache.fs.Archive;
 import com.sk.cache.fs.CacheSystem;
-import com.sk.cache.fs.CacheType;
-import com.sk.cache.fs.FileData;
-import com.sk.datastream.ConcatenatedStream;
 import com.sk.datastream.Stream;
 
-public class LocalObjectLoader extends WrapperLoader {
-
-	private final CacheType cache;
+public class LocalObjectLoader extends RegionDataLoader<LocalObjects> {
 
 	public LocalObjectLoader(CacheSystem cacheSystem) {
 		super(cacheSystem);
-		this.cache = cacheSystem.getCacheType(5);
 	}
 
 	@Override
 	public LocalObjects load(int regionHash) {
-		Stream data = getData(regionHash);
+		Stream data = getData(regionHash, 0, 1, 2);
 		LocalObjects ret = new LocalObjects(this, regionHash);
 		ret.decode(data);
 		return ret;
 	}
-
-	public Stream getData(int regionHash) {
-		Archive archive = cache.getArchive(regionHash);
-		if (archive == null)
-			throw new IllegalArgumentException("Bad region hash");
-		List<Stream> streams = new ArrayList<>();
-		for (int i = 0; i < 3; ++i) {
-			FileData file = archive.getFile(i);
-			if (file == null)
-				break;
-			streams.add(file.getDataAsStream());
-		}
-		return new ConcatenatedStream(streams.toArray(new Stream[streams.size()]));
-	}
-
-	public LocalObjects load(int regionX, int regionY) {
-		return load((regionX & 0x3f) << 7 | (regionY & 0x3f));
-	}
-
 }
---
+++ src/com/sk/wrappers/RegionDataLoader.java
@@ -0,0 +1,47 @@
+package com.sk.wrappers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.sk.cache.fs.Archive;
+import com.sk.cache.fs.CacheSystem;
+import com.sk.cache.fs.CacheType;
+import com.sk.cache.fs.FileData;
+import com.sk.datastream.ConcatenatedStream;
+import com.sk.datastream.Stream;
+
+
+public abstract class RegionDataLoader<T extends Wrapper<?>> extends WrapperLoader {
+
+	protected final CacheType cache;
+
+	public RegionDataLoader(CacheSystem cacheSystem) {
+		super(cacheSystem);
+		this.cache = cacheSystem.getCacheType(5);
+	}
+	
+	@Override
+	public abstract T load(int regionHash);
+	
+	public T load(int regionX, int regionY) {
+		if (regionX < 0 || regionY < 0 || regionX >= 0x3f || regionY >= 0x3f) {
+			throw new IllegalArgumentException("Bad region coordinates");
+		}
+		return load(regionX << 7 | regionY);
+	}
+
+	protected Stream getData(int regionHash, int... files) {
+		Archive archive = cache.getArchive(regionHash);
+		if (archive == null)
+			throw new IllegalArgumentException("Bad region hash");
+		List<Stream> streams = new ArrayList<>();
+		for (int file : files) {
+			FileData data = archive.getFile(file);
+			if (data == null)
+				continue;
+			streams.add(data.getDataAsStream());
+		}
+		return new ConcatenatedStream(streams.toArray(new Stream[streams.size()]));
+	}
+
+}
