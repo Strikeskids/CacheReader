b1ae89c0eddf1307365ab9e9e31bfe27583d2b4c
Strikeskids <github@strikeskids.com>
2013-12-16 22:17:16 -0500

    Made protocol packer handle values nicely

--- src/com/sk/dist/ProtocolPacker.java
+++ src/com/sk/dist/ProtocolPacker.java
@@ -55,19 +55,21 @@ public class ProtocolPacker<T extends Packed> extends Packer<T> {
 
 	private int packField(OutputStream out, Object value) throws IOException {
 		int ret = 0;
-		if (value == null)
-			value = "";
-		Class<?> type = value.getClass();
-		if (ProtocolType.ARRAY.isType(type)) {
-			int len = Array.getLength(value);
-			ret += writeValue(out, Array.getLength(value));
-			for (int i = 0; i < len; ++i) {
-				ret += packField(out, Array.get(value, i));
+		if (value == null) {
+			ret += writeValue(out, 0);
+		} else {
+			Class<?> type = value.getClass();
+			if (ProtocolType.ARRAY.isType(type)) {
+				int len = Array.getLength(value);
+				ret += writeValue(out, Array.getLength(value));
+				for (int i = 0; i < len; ++i) {
+					ret += packField(out, Array.get(value, i));
+				}
+			} else if (ProtocolType.INTEGER.isType(type)) {
+				ret += writeValue(out, (long) value);
+			} else if (ProtocolType.STRING.isType(type)) {
+				ret += writeString(out, String.valueOf(value));
 			}
-		} else if (ProtocolType.INTEGER.isType(type)) {
-			ret += writeValue(out, (long) value);
-		} else if (ProtocolType.STRING.isType(type)) {
-			ret += writeString(out, String.valueOf(value));
 		}
 		return ret;
 	}
