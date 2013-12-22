9b91e7f568778df8c2839f343799c35dae0efe0c
Strikeskids <github@strikeskids.com>
2013-12-22 13:09:55 -0500

    Added pack driver

---
+++ src/com/sk/cache/dist/pack/PackDriver.java
@@ -0,0 +1,37 @@
+package com.sk.cache.dist.pack;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+import com.sk.cache.DataSource;
+import com.sk.cache.fs.CacheSystem;
+import com.sk.cache.wrappers.ItemDefinitionLoader;
+import com.sk.cache.wrappers.ObjectDefinitionLoader;
+import com.sk.cache.wrappers.region.RegionLoader;
+
+public class PackDriver {
+
+	private static ZipOutputStream zos;
+
+	public static void main(String[] args) throws Exception {
+		long start = System.currentTimeMillis();
+		CacheSystem sys = new CacheSystem(new DataSource(new File("/Users/Strikeskids/jagexcache/Runescape/LIVE")));
+		File out = new File("packed" + System.currentTimeMillis() + ".zip");
+		zos = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(out)));
+		pack(new RegionPacker(new RegionLoader(sys)));
+		pack(new ObjectPacker(new ObjectDefinitionLoader(sys)));
+		pack(new ItemPacker(new ItemDefinitionLoader(sys)));
+		zos.close();
+		System.out.println("Finished all packing");
+		System.out.println("Took " + (System.currentTimeMillis() - start) + " millis");
+	}
+
+	private static void pack(Packer<?> pckr) throws IOException {
+		zos.putNextEntry(new ZipEntry(pckr.getStorage().getSimpleName() + ".packed"));
+		pckr.pack(zos);
+		zos.closeEntry();
+	}
+}
