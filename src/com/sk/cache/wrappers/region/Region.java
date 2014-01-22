e820d13b7f5f143317b1ad014c659f03b329066e
Strikeskids <github@strikeskids.com>
2014-01-22 08:03:45 -0500

    Added storing extra data from cache

--- src/com/sk/cache/wrappers/region/Region.java
+++ src/com/sk/cache/wrappers/region/Region.java
@@ -11,6 +11,8 @@ public class Region extends StreamedWrapper {
 	public LocalObjects objects;
 	public final static int width = 64;
 	public final static int height = 64;
+	
+	public int[][][][] extraData = new int[4][width][height][5];
 
 	public Region(RegionLoader loader, int id) {
 		super(loader, id);
@@ -19,19 +21,19 @@ public class Region extends StreamedWrapper {
 	@Override
 	public void decode(Stream stream) {
 		for (int plane = 0; plane < 4; plane++) {
-			for (int x = 0; x < 64; x++) {
-				for (int y = 0; y < 64; y++) {
-					int operation = stream.getUByte();
+			for (int x = 0; x < width; x++) {
+				for (int y = 0; y < height; y++) {
+					int operation = extraData[plane][x][y][4] = stream.getUByte();
 					if ((operation & 0x1) != 0) {
-						stream.getUByte();
-						stream.getSmart();
+						extraData[plane][x][y][0] = stream.getUByte();
+						extraData[plane][x][y][1] = stream.getSmart();
 					}
 					if ((operation & 0x2) != 0)
 						landscapeData[plane][x][y] = stream.getByte();
 					if ((operation & 0x4) != 0)
-						stream.getSmart();
+						extraData[plane][x][y][2] = stream.getSmart();
 					if ((operation & 0x8) != 0)
-						stream.getUByte();
+						extraData[plane][x][y][3] = stream.getUByte();
 				}
 			}
 		}
