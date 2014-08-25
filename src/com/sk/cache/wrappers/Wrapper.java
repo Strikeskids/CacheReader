7404e5213172574cc9e23d3ed87b6affdf9ea57d
Strikeskids <github@strikeskids.com>
2014-08-25 17:31:40 -0400

    Improved logging and display in ProtocolWrapper

--- src/com/sk/cache/wrappers/Wrapper.java
+++ src/com/sk/cache/wrappers/Wrapper.java
@@ -3,12 +3,17 @@ package com.sk.cache.wrappers;
 import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
+import java.util.Collection;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import com.sk.cache.wrappers.loaders.WrapperLoader;
 
 public abstract class Wrapper {
+
+	protected static final Logger WRAPPER_LOGGER = Logger.getLogger(Wrapper.class.getCanonicalName());
 
 	protected final WrapperLoader<?> loader;
 	protected final int id;
@@ -68,15 +73,42 @@ public abstract class Wrapper {
 		return output.toString();
 	}
 
+	protected void skipValue(int opcode, Object... os) {
+		StringBuilder builder = new StringBuilder();
+		builder.append(opcode);
+		builder.append(" ");
+		appendObject(builder, os != null && os.length == 1 ? os[0] : os);
+		WRAPPER_LOGGER.logp(Level.FINE, this.getClass().getName(), "skip", builder.toString());
+	}
+
 	private void appendObject(StringBuilder output, Object o) {
-		if (o == null)
-			return;
-		if (o.getClass().isArray()) {
+		if (o == null) {
+			output.append("null");
+		} else if (o instanceof Long) {
+			output.append(String.format("%016x", ((Long) o).longValue()));
+		} else if (o instanceof Double) {
+			output.append(o);
+		} else if (o instanceof Float) {
+			output.append(o);
+		} else if (o instanceof Number) {
+			output.append(String.format("%08x", ((Number) o).intValue()));
+		} else if (o instanceof Boolean) {
+			output.append((Boolean) o ? "T" : "F");
+		} else if (o.getClass().isArray()) {
 			output.append("[");
 			for (int i = 0, len = Array.getLength(o); i < len; ++i) {
 				if (i != 0)
 					output.append(", ");
 				appendObject(output, Array.get(o, i));
+			}
+			output.append("]");
+		} else if (o instanceof Collection<?>) {
+			output.append("[");
+			boolean first = true;
+			for (Object child : (Collection<?>) o) {
+				if (first & !(first = false))
+					output.append(", ");
+				appendObject(output, child);
 			}
 			output.append("]");
 		} else {
